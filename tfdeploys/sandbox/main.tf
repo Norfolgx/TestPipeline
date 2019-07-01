f6ff7f75f5142eda4bbe9134add1ba8e4f8e9bbd
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-01 19:58:30 +0100

    Fixed the last var errors.

--- tfdeploys/sandbox/main.tf
+++ tfdeploys/sandbox/main.tf
@@ -7,11 +7,12 @@ provider "aws" {
 
 module "ec2" {
   source = "../../tfmodules/ec2"
+  vpc_id = "${var.vpc_id}"
   subnet_id = "${var.subnet_id}"
   app_name = "${var.app_name}"
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
-  key_name = "${var.keyname}"
+  key_name = "${var.key_name}"
   instance_profile = "${var.instance_profile}"
   guid = "${var.guid}"
   asset_id = "${var.asset_id}"
