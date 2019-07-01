4bd27fd713a243a487f2758985ab8fa445677c17
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-01 20:44:46 +0100

    Syntax fix for region.

--- tfdeploys/sandbox/main.tf
+++ tfdeploys/sandbox/main.tf
@@ -6,10 +6,10 @@
 # }
 
 provider "aws" {
+  region = "${var.region}"
   assume_role {
     role_arn = "${var.role_arn}"
     session_name = "${var.session_name}"
-    region = "${var.region}"
   }
 }
 
