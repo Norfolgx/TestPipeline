171b48c845db0e491f1ab7795e5794adbb7bf702
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-01 19:49:58 +0100

    Corrected var names with app in them.

--- tfmodules/ec2/main.tf
+++ tfmodules/ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "ec2" {
-  subnet_id = "${var.app_subnet_id}"
-  ami = "${var.app_ami}"
-  instance_type = "${var.app_instance_type}"
-  key_name = "${var.app_keyname}"
+  subnet_id = "${var.subnet_id}"
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.keyname}"
   security_groups = ["${aws_security_group.app.id}"]
-  iam_instance_profile = "${var.app_instance_profile}"
+  iam_instance_profile = "${var.instance_profile}"
 
   tags {
     Name = "${var.app_name}"
