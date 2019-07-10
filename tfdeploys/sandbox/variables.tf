9d39a65fe3d246f33b51f0f8048b3f5f42ac4962
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-10 13:13:31 +0100

    Changed tag name to be generic for ami retrevial(sp).

--- tfdeploys/sandbox/variables.tf
+++ tfdeploys/sandbox/variables.tf
@@ -3,7 +3,6 @@ variable "session_name" {}
 variable "region" {}
 variable "vpc_id" { default = "vpc-fa0cee9f" }
 variable "app_name" { default = "GeorgeTest" }
-# variable "ami" {}
 variable "instance_type" { default = "t2.micro" }
 variable "subnet_id" { default = "subnet-893fc7fe" }
 variable "key_name" { default = "lnuk-lns-devops" }
