e483e8f993d441d4120294f23126e1923c7fc6bb
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-12 16:43:31 +0100

    Changed protocol to http.

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -14,14 +14,14 @@ resource "aws_lb" "app" {
 resource "aws_lb_target_group" "app" {
   name     = "${var.app_name}-tg"
   port     = 80
-  protocol = "TCP"
+  protocol = "HTTP"
   vpc_id   = "${var.vpc_id}"
 }
 
 resource "aws_lb_listener" "app" {
   load_balancer_arn = "${aws_lb.app.arn}"
   port              = "80"
-  protocol          = "TCP"
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.app.arn}"
