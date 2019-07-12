f617ec95c0c82c225545142b2788ca7e4b43adb0
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-12 15:07:41 +0100

    Calling alb module.

--- terraform/deploys/sandbox/main.tf
+++ terraform/deploys/sandbox/main.tf
@@ -27,3 +27,16 @@ module "ec2" {
   guid = "${var.guid}"
   asset_id = "${var.asset_id}"
 }
+
+module "alb" {
+  source = "../../modules/alb"
+  vpc_id = "${var.vpc_id}"
+  subnet_id = "${var.subnet_id}"
+  app_name = "${var.app_name}"
+  ami = "${data.aws_ami.image.id}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  instance_profile = "${var.instance_profile}"
+  guid = "${var.guid}"
+  asset_id = "${var.asset_id}"
+}
