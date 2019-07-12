32378ae39bffab64848fe2400a72dbfb43f438b6
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-12 16:18:17 +0100

    Added alb listener and target group.

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -11,6 +11,23 @@ resource "aws_lb" "app" {
   }
 }
 
+resource "aws_lb_target_group" "app" {
+  name     = "${var.app_name}-tg"
+  port     = 5000
+  protocol = "tcp"
+  vpc_id   = "${var.vpc_id}"
+}
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = "${aws_lb.app.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.app.arn}"
+  }
+}
+
 resource "aws_security_group" "alb" {
   name = "${var.app_name}-alb-sg"
   vpc_id = "${var.vpc_id}"
