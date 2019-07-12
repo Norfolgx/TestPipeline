2cbf15d94809d816d372238f2c303266d72ed1f7
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-12 15:38:50 +0100

    Removed list type specifier.

--- terraform/deploys/sandbox/variables.tf
+++ terraform/deploys/sandbox/variables.tf
@@ -9,7 +9,4 @@ variable "key_name" { default = "lnuk-lns-devops" }
 variable "instance_profile" { default = "App" }
 variable "guid" { default = "7e1ae8e8-a94c-495e-8fe7-e8aaff258b22" }
 variable "asset_id" { default = "1461" }
-variable "public_subnets" {
-  type    = list(string)
-  default = ["subnet-0f3070eed18efff29", "subnet-063b32540e35f5680", "subnet-08a3497f0b7ad16fc"]
-}
+variable "public_subnets" { default = ["subnet-0f3070eed18efff29", "subnet-063b32540e35f5680", "subnet-08a3497f0b7ad16fc"] }
