52dd7f33dad2748448bfda681429b772979f4196
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-07-12 15:02:10 +0100

    Added alb module test.

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,58 @@
+resource "aws_instance" "ec2" {
+  subnet_id = "${var.subnet_id}"
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  security_groups = ["${aws_security_group.app.id}"]
+  iam_instance_profile = "${var.instance_profile}"
+  user_data = <<EOF
+#!/bin/bash
+serve -s /home/ec2-user/build
+EOF
+  tags {
+    Name = "${var.app_name}"
+    GUID = "${var.guid}"
+    AssetID = "${var.asset_id}"
+  }
+}
+
+resource "aws_security_group" "app" {
+  name = "${var.app_name}-SG"
+  vpc_id = "${var.vpc_id}"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["172.29.0.0/16"]
+  }
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "${var.app_name}"
+    GUID = "${var.guid}"
+    AssetID = "${var.asset_id}"
+  }
+}
+
+resource "aws_lb" "app" {
+  name               = "${var.app_name}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.alb.id}"]
+  subnets            = ["${aws_subnet.public.*.id}"]
+  tags {
+    Name = "${var.app_name}"
+    GUID = "${var.guid}"
+    AssetID = "${var.asset_id}"
+  }
+}
