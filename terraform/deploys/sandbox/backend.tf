c9146d3137448ca6a2ef8a37f43b81e363eef988
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2019-08-12 15:58:57 +0100

    Fixing DevOps bucket backend.

--- terraform/deploys/sandbox/backend.tf
+++ terraform/deploys/sandbox/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "lnuk-devops-tfstate"
-    key = "GeorgeTestPipeline/sandbox.tfstate"
+    key = "george-test-pipeline/george-test-pipeline-sandbox.tfstate"
     region = "eu-west-1"
     encrypt = true
   }
